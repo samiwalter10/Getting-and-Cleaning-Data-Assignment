526692683c53b23c2b8675e0d9a84556349e5a9a
samiwalter10 <39140647+samiwalter10@users.noreply.github.com>
2018-12-30 11:30:19 -0800

    Add files via upload

---
+++ run_analysis.R
@@ -0,0 +1,104 @@
+# Course: Getting and Cleaning Data   
+# Goal: Create a clean and tidy data set
+# Output: "tidy_data.txt"
+# See README.md for details.
+
+library(dplyr)
+
+## 1. Get Data
+
+# download zip file containing data if it hasn't already been downloaded
+zipUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+zipFile <- "UCI HAR Dataset.zip"
+
+if (!file.exists(zipFile)) {
+  download.file(zipUrl, zipFile, mode = "wb")
+}
+
+# unzip zip file containing data if data directory doesn't already exist
+dataPath <- "UCI HAR Dataset"
+if (!file.exists(dataPath)) {
+  unzip(zipFile)
+}
+
+## 2. Read Data
+
+# read training data
+trainSubjects <- read.table(file.path(dataPath, "train", "subject_train.txt"))
+trainValues <- read.table(file.path(dataPath, "train", "X_train.txt"))
+trainActivity <- read.table(file.path(dataPath, "train", "y_train.txt"))
+
+# read test data
+testSubjects <- read.table(file.path(dataPath, "test", "subject_test.txt"))
+testValues <- read.table(file.path(dataPath, "test", "X_test.txt"))
+testActivity <- read.table(file.path(dataPath, "test", "y_test.txt"))
+
+# read features, don't convert text labels to factors
+features <- read.table(file.path(dataPath, "features.txt"), as.is = TRUE)
+
+# read activity labels
+activities <- read.table(file.path(dataPath, "activity_labels.txt"))
+colnames(activities) <- c("activityId", "activityLabel")
+
+
+## 3. Merge the training and the test sets to create one data set
+
+# concatenate individual data tables to make single data table
+humanActivity <- rbind(
+  cbind(trainSubjects, trainValues, trainActivity),
+  cbind(testSubjects, testValues, testActivity)
+)
+
+# remove individual data tables to save memory
+rm(trainSubjects, trainValues, trainActivity, 
+   testSubjects, testValues, testActivity)
+
+# assign column names
+colnames(humanActivity) <- c("subject", features[, 2], "activity")
+
+## 4. Extract only the measurements on the mean and standard deviation for each measurement
+
+# keep relevant columns
+RelevantColumns <- grepl("subject|activity|mean|std", colnames(humanActivity))
+humanActivity <- humanActivity[, RelevantColumns]
+
+## 5. Rename activities with descriptive activity names
+
+# replace activity values with named factor levels
+humanActivity$activity <- factor(humanActivity$activity,levels = activities[, 1], labels = activities[, 2])
+
+# get column names
+humanActivityCols <- colnames(humanActivity)
+
+# remove special characters
+humanActivityCols <- gsub("[\\(\\)-]", "", humanActivityCols)
+
+# expand abbreviations and clean up names
+humanActivityCols <- gsub("^f", "frequencyDomain", humanActivityCols)
+humanActivityCols <- gsub("^t", "timeDomain", humanActivityCols)
+humanActivityCols <- gsub("Acc", "Accelerometer", humanActivityCols)
+humanActivityCols <- gsub("Gyro", "Gyroscope", humanActivityCols)
+humanActivityCols <- gsub("Mag", "Magnitude", humanActivityCols)
+humanActivityCols <- gsub("Freq", "Frequency", humanActivityCols)
+humanActivityCols <- gsub("mean", "Mean", humanActivityCols)
+humanActivityCols <- gsub("std", "StandardDeviation", humanActivityCols)
+
+# correct typo
+humanActivityCols <- gsub("BodyBody", "Body", humanActivityCols)
+
+# use new labels as column names
+colnames(humanActivity) <- humanActivityCols
+
+## 6. Create a data set with the average of each variable for each activity and each subject
+
+# group by subject and activity and summarise using mean
+humanActivityMeans <- humanActivity %>% 
+  group_by(subject, activity) %>%
+  summarise_each(funs(mean))
+
+# output to file "tidy_data.txt"
+write.table(humanActivityMeans, "tidy_data.txt", row.names = FALSE, 
+            quote = FALSE)
+
+
+
